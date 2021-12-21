1f3c9254cdde236c54e543c7c732a183feacee9a
elio <elio_1@qq.com>
2021-12-21 17:27:34 +0800

    这是master的第2次提交

--- users/src/main/java/com/example/users/controller/UserController.java
+++ users/src/main/java/com/example/users/controller/UserController.java
@@ -3,6 +3,7 @@ package com.example.users.controller;
 import com.example.users.domain.User;
 import com.example.users.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,9 +14,12 @@ public class UserController {
     @Autowired
     private UserMapper userMapper;
 
+    @Value("${spring.datasource.username}")
+    private String username;
+
     @RequestMapping("/user")
     public List<User> findAll(){
-        System.err.println("user1");
+        System.err.println("user1" + username);
         return userMapper.findAll();
     }
 }
